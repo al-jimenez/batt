b07ee2d2d96593e5ce3d43e02ee74a29aa6d20cc
Al Jimenez <al.jimenez@mail.com>
2020-08-24 15:23:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# batt()
+This is a simple function that displays the status of the MacBook Pro's battery.  
+
+## Use
+Use this and customize it with your changes. I use this as to check on my battery cycle count and % life cycle time left to it.
+I just recently replaced my MacBook Pro's battery becasue it has too many cycles on it so I wanted to track its status.
+
+## OS
+Very specific to macOS and MacBook Pro.  Uses system_profiler calls to get info.
+
+## Help menu
+When you execute batt it will return the following information:
+![Output]()
---
+++ batt.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#batt() {
+  batt_install_date=$(date -j -f "%b %d %Y %H:%M:%S" "Jul 11 2020 08:00:00" +%s)        # date arithmatic
+  now=$(date +%s)                                                                       # today's date
+  days_installed=$(printf '%d days' "$(( (now-batt_install_date)/86400 ))")             # number of days installed since 7/11/2020
+  CYCLECOUNT=`system_profiler SPPowerDataType | awk '/Cycle Count:/ { print $0 }' | cut -f 2 -d ":" | squeeze`
+  CHARGE=`system_profiler SPPowerDataType | awk '/Charge Remaining/ { print $0 }' | cut -f 2 -d ":" | squeeze`
+  FULLYCHARGED=`system_profiler SPPowerDataType | awk '/Fully Charged:/ { print $3 }' | squeeze`
+  FULLYCHARGCAP=`system_profiler SPPowerDataType | awk '/Full Charge Capacity/ { print $5 }' | squeeze`
+  CONDITION=`system_profiler SPPowerDataType | awk '/Condition/ { print $2 }' | squeeze`
+  percent_life=$(printf '%.0f' "$((100*${CYCLECOUNT#0}/1500))")
+  echo;
+  echo " Battery Information: "
+  echo "  Condition: ${CONDITION}"
+  echo "  Fully Charged: ${FULLYCHARGED}"
+  echo "  Charge Remaining (mAh): ${CHARGE}";
+  echo "  Full Charge Capacity (mAh): ${FULLYCHARGCAP}"
+  echo "  Cycle Count: ${CYCLECOUNT} (${percent_life}% of life cycle)"
+  echo "  Battery replaced ${days_installed} days ago (July 11, 2020)" ;
+  echo;
+#}
